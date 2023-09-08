7946f15b017b61044cbffc3a639fe8891dad855a
giovannitamburini <giovatambu@gmail.com>
2023-09-08 16:42:54 +0200

    completed exercise without bonus

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Product
+    {
+        // ATTRIBUTI
+        private int code;
+        public int Code
+        {
+            get
+            {
+                return code;
+            }
+        }
+
+        private string name = "prodotto";
+        public string Name
+        {
+            get
+            { 
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string description = "descrizione";
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        private float price;
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        private float vat = 0.22f;
+        public float Vat
+        {
+            get
+            {
+                return vat;
+            }
+        }
+
+        // STATI
+
+
+        // COSTRUTTORI
+
+        public Product(string nameProduct, string descriptionproduct, float price)
+        {
+            this.code = randomNumberGeneratorFrom1toN(100000000);
+            this.Name = nameProduct;
+            this.Description = descriptionproduct;
+            this.Price = price;
+        }
+
+
+        // GETTER
+
+
+        // SETTER
+
+
+        // METODI
+
+        // metodo per creare un numero intero casuale
+        public int randomNumberGeneratorFrom1toN (int maxNumber)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, maxNumber);
+
+            return randomNumber;
+        }
+
+        // metodo per stampare a video il prezzo senza iva
+        public void PriceWithoutVat()
+        {
+            Console.WriteLine($"Il prezzo del prodotto '{this.Name}' senza iva è di {Price} euro");
+        }
+
+        // metodo per stampare a video il prezzo con iva
+        public void PriceWithVat()
+        {
+            float priceWithVat = this.Price + (this.Price * this.Vat);
+            Console.WriteLine($"Il prezzo del prodotto '{this.Name}' con iva è di {priceWithVat} euro");
+        }
+
+        // metodo per avere il nome esteso, ottenuto concatenando codice + nome
+        public string ConcatenatedName()
+        {
+            string codeForName = this.Code.ToString();
+
+            string concatenatedName = codeForName + this.Name;
+
+            return concatenatedName;
+        }
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            
+            // istanzio 3 oggetti della Classe Product
+            Product product1 = new Product("maglietta", "maglietta soffice in cotone a maniche corte", 10.0f);
+            Product product2 = new Product("pantaloni", "pantaloni super", 40.99f);
+            Product product3 = new Product("felpa", "felpa molto calda", 35.99f);
+
+            // stampo a video il nome del prodotto, il suo codice e la sua descrizione
+            Console.WriteLine($"Il nome del prodotto è: {product1.Name}");
+            Console.WriteLine($"Il codice del prodotto {product1.Name} è: {product1.Code}");
+            Console.WriteLine($"La descrizione del prodotto {product1.Name} è: {product1.Description}");
+
+            // stampo a video il prezzo senza iva
+            Console.Write($"Il prezzo senza iva del prodotto {product1.Name} è: ");
+            product1.PriceWithoutVat();
+
+            // stampo a video il prezzo con iva
+            Console.Write($"Il prezzo con iva del prodotto {product1.Name} è: ");
+            product1.PriceWithVat();
+
+            // Stampo a video il nome concatenato
+            string concatenatedNameProduct1 = product1.ConcatenatedName();
+            Console.WriteLine($"Il nome concatenato del prodotto {product1.Name} è: ");
+            Console.WriteLine(concatenatedNameProduct1);
+
+            // PRODOTTO 2 ----------------
+            // stampo a video il nome del prodotto, il suo codice e la sua descrizione
+            Console.WriteLine($"Il nome del prodotto è: {product2.Name}");
+            Console.WriteLine($"Il codice del prodotto {product2.Name} è: {product2.Code}");
+            Console.WriteLine($"La descrizione del prodotto {product2.Name} è: {product2.Description}");
+
+            // stampo a video il prezzo senza iva
+            Console.Write($"Il prezzo senza iva del prodotto {product2.Name} è: ");
+            product2.PriceWithoutVat();
+
+            // stampo a video il prezzo con iva
+            Console.Write($"Il prezzo con iva del prodotto {product2.Name} è: ");
+            product2.PriceWithVat();
+
+            // Stampo a video il nome concatenato
+            string concatenatedNameProduct2 = product2.ConcatenatedName();
+            Console.WriteLine($"Il nome concatenato del prodotto {product2.Name} è: ");
+            Console.WriteLine(concatenatedNameProduct2);
         }
     }
 }
